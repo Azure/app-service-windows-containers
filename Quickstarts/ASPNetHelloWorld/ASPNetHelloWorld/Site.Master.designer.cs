cd797782568875e60d5a216adf1176204f6f27d7
Andrew Westgarth <mail@hawaythelads.co.uk>
2019-03-13 13:33:57 -0700

    Updated Site Master page to fix issues since bootstrap upgrade and also to set application title

--- Quickstarts/ASPNetHelloWorld/ASPNetHelloWorld/Site.Master.designer.cs
+++ Quickstarts/ASPNetHelloWorld/ASPNetHelloWorld/Site.Master.designer.cs
@@ -7,22 +7,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ASPNetHelloWorld
-{
-
-
-    public partial class SiteMaster
-    {
-
-        /// <summary>
-        /// HeadContent control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
-
+namespace ASPNetHelloWorld {
+    
+    
+    public partial class SiteMaster {
+        
         /// <summary>
         /// MainContent control.
         /// </summary>
